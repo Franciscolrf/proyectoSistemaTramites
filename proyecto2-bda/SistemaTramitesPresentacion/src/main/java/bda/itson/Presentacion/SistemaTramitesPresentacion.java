89937174435e9076bf46aa4b1afcda32afc12415
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-03-20 03:48:32 -0700

    Creación github

---
+++ proyecto2-bda/SistemaTramitesPresentacion/src/main/java/bda/itson/Presentacion/SistemaTramitesPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bda.itson.Presentacion;
+
+/**
+ *
+ * @author ID145
+ */
+public class SistemaTramitesPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
