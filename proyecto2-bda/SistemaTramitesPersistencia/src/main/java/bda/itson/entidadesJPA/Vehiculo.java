b4480a8551ee7994022267426f4caa25111065bd
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-03-21 04:24:30 -0700

    Implementación vehiculo
    Implementación de la entidad Vehículo y documentación

--- proyecto2-bda/SistemaTramitesPersistencia/src/main/java/bda/itson/entidadesJPA/Vehiculo.java
+++ proyecto2-bda/SistemaTramitesPersistencia/src/main/java/bda/itson/entidadesJPA/Vehiculo.java
@@ -14,14 +14,15 @@ import javax.persistence.*;
  * @author ID145
  */
 @Entity
+@Table(name = "vehiculo")
 public class Vehiculo implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "numeroSerie")
     private Long id;
 
-    private String numeroSerie;
     private String estado;
     private String color;
     private String modelo;
@@ -35,78 +36,181 @@ public class Vehiculo implements Serializable {
     @OneToMany(mappedBy = "vehiculo")
     private List<Placa> placas;
 
+    /**
+     * Constructor vacío
+     */
+    public Vehiculo() {
+    }
+
+    /**
+     * Constructor que recibe todo, excepto el id
+     * @param estado
+     * @param color
+     * @param modelo
+     * @param marca
+     * @param linea
+     * @param propietario
+     * @param placas
+     */
+    public Vehiculo(String estado, String color, String modelo, String marca, String linea, Persona propietario, List<Placa> placas) {
+        this.estado = estado;
+        this.color = color;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.linea = linea;
+        this.propietario = propietario;
+        this.placas = placas;
+    }
+
+    /**
+     * Constructor que recibe todo, excepto el id
+     * @param estado
+     * @param color
+     * @param modelo
+     * @param marca
+     * @param linea
+     * @param propietario
+     */
+    public Vehiculo(String estado, String color, String modelo, String marca, String linea, Persona propietario) {
+        this.estado = estado;
+        this.color = color;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.linea = linea;
+        this.propietario = propietario;
+    }
+    /**
+     * Método que regresa el id del vehículo
+     * @return
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Método que recibe el id del vehículo
+     * @param id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getNumeroSerie() {
-        return numeroSerie;
-    }
-
-    public void setNumeroSerie(String numeroSerie) {
-        this.numeroSerie = numeroSerie;
-    }
-
+    /**
+     * Método que regresa el estado del vehículo
+     * @return
+     */
     public String getEstado() {
         return estado;
     }
 
+    /**
+     * Método que establece el estado del vehículo
+     * @param estado
+     */
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
+    /**
+     * Método que regresa el color del vehículo
+     * @return
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     * Método que establece el color del vehículo
+     * @param color
+     */
     public void setColor(String color) {
         this.color = color;
     }
 
+    /**
+     * Método que regresa el modelo del vehículo
+     * @return
+     */
     public String getModelo() {
         return modelo;
     }
 
+    /**
+     * Método que establece el modelo del vehículo
+     * @param modelo
+     */
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
+    /**
+     * Método que regresa la marca del vehículo
+     * @return
+     */
     public String getMarca() {
         return marca;
     }
 
+    /**
+     * Método que establece la marca del vehículo
+     * @param marca
+     */
     public void setMarca(String marca) {
         this.marca = marca;
     }
 
+    /**
+     * Método que regresa la línea del vehículo
+     * @return
+     */
     public String getLinea() {
         return linea;
     }
 
+    /**
+     * Método que establece la línea del vehículo
+     * @param linea
+     */
     public void setLinea(String linea) {
         this.linea = linea;
     }
 
+    /**
+     * Método que regresa el propietario
+     * @return
+     */
     public Persona getPropietario() {
         return propietario;
     }
 
+    /**
+     * Método que establece el propietario
+     * @param propietario
+     */
     public void setPropietario(Persona propietario) {
         this.propietario = propietario;
     }
 
+    /**
+     * Método que regresa las placas
+     * @return
+     */
     public List<Placa> getPlacas() {
         return placas;
     }
 
+    /**
+     * Método que establece las placas
+     * @param placas
+     */
     public void setPlacas(List<Placa> placas) {
         this.placas = placas;
     }
 
+    /*
+     * Método que regresa el hash
+     * @return hash
+     */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -114,6 +218,11 @@ public class Vehiculo implements Serializable {
         return hash;
     }
 
+    /*
+     * Método que regresa si dos objetos son iguales
+     * @param object
+     * @return true si son iguales, false si no
+     */
     @Override
     public boolean equals(Object object) {
         if (!(object instanceof Vehiculo)) {
@@ -126,9 +235,17 @@ public class Vehiculo implements Serializable {
         return true;
     }
 
+    /*
+     * Método que regresa el objeto en forma de string
+     * @return cadena
+     */
     @Override
     public String toString() {
-        return "bda.itson.entidadesJPA.Vehiculo[ id=" + id + " ]";
+        return "Vehiculo [id=" + id + ", estado=" + estado + ", color=" + color + ", modelo=" + modelo + ", marca="
+                + marca + ", linea=" + linea + ", propietario=" + propietario + ", placas=" + placas + "]";
     }
+
+    
+
     
 }
