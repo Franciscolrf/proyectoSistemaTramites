0808357f549a6d2726c3d84aedfeee02b1bdf632
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-03-21 03:30:59 -0700

    Update Persona.java
    Modificación getters, setters y comentarios.

--- proyecto2-bda/SistemaTramitesPersistencia/src/main/java/bda/itson/entidadesJPA/Persona.java
+++ proyecto2-bda/SistemaTramitesPersistencia/src/main/java/bda/itson/entidadesJPA/Persona.java
@@ -37,86 +37,228 @@ public class Persona implements Serializable {
     @OneToMany(mappedBy = "propietario", cascade = CascadeType.ALL)
     private List<Vehiculo> vehiculos;
 
+
+    /**
+     * Constructor que recibe todo, excepto el id
+     * @param fechaNacimiento
+     * @param esDiscapacitado
+     * @param telefono
+     * @param nombres
+     * @param apellidoPaterno
+     * @param apellidoMaterno
+     * @param licencias
+     * @param vehiculos
+     */
+    public Persona(Calendar fechaNacimiento, boolean esDiscapacitado, String telefono, String nombres, String apellidoPaterno, String apellidoMaterno, List<Licencia> licencias, List<Vehiculo> vehiculos) {
+        this.fechaNacimiento = fechaNacimiento;
+        this.esDiscapacitado = esDiscapacitado;
+        this.telefono = telefono;
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.licencias = licencias;
+        this.vehiculos = vehiculos;
+    }
+
+    /**
+     * Constructor que recibe todo, excepto ID y lista de licencias
+     * @param fechaNacimiento
+     * @param esDiscapacitado
+     * @param telefono
+     * @param nombres
+     * @param apellidoPaterno
+     * @param apellidoMaterno
+     * @param vehiculos
+     */
+    public Persona(Calendar fechaNacimiento, boolean esDiscapacitado, String telefono, String nombres, String apellidoPaterno, String apellidoMaterno, List<Vehiculo> vehiculos) {
+        this.fechaNacimiento = fechaNacimiento;
+        this.esDiscapacitado = esDiscapacitado;
+        this.telefono = telefono;
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.vehiculos = vehiculos;
+    }
+
+    
+
+    /**
+     * Constructor que recibe todo.
+     * @param id
+     * @param fechaNacimiento
+     * @param esDiscapacitado
+     * @param telefono
+     * @param nombres
+     * @param apellidoPaterno
+     * @param apellidoMaterno
+     * @param licencias
+     * @param vehiculos
+     */
+    public Persona(Long id, Calendar fechaNacimiento, boolean esDiscapacitado, String telefono, String nombres, String apellidoPaterno, String apellidoMaterno, List<Licencia> licencias, List<Vehiculo> vehiculos) {
+        this.id = id;
+        this.fechaNacimiento = fechaNacimiento;
+        this.esDiscapacitado = esDiscapacitado;
+        this.telefono = telefono;
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.licencias = licencias;
+        this.vehiculos = vehiculos;
+    }
+
+    /**
+     * Constructor vacío
+     */
+    public Persona() {
+    }
+    /**
+     * Método que regresa el id de la persona
+     * @return id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Método que establece el id de la persona
+     * @param id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getRFC() {
-        return RFC;
-    }
-
-    public void setRFC(String RFC) {
-        this.RFC = RFC;
-    }
-
+    /**
+     * Método que regresa la fecha de nacimiento
+     * @return fechaNacimiento
+     */
     public Calendar getFechaNacimiento() {
         return fechaNacimiento;
     }
 
+    /**
+     * Método que establece la fecha de nacimiento
+     * @param fechaNacimiento
+     */
     public void setFechaNacimiento(Calendar fechaNacimiento) {
         this.fechaNacimiento = fechaNacimiento;
     }
 
+    /**
+     * Método que regresa si es discapacitado
+     * @return esDiscapacitado
+     */
     public boolean isEsDiscapacitado() {
         return esDiscapacitado;
     }
 
+    /**
+     * Método que establece si es discapacitado
+     * @param esDiscapacitado
+     */
     public void setEsDiscapacitado(boolean esDiscapacitado) {
         this.esDiscapacitado = esDiscapacitado;
     }
 
+    /**
+     * Método que regresa el teléfono
+     * @return telefono
+     */
     public String getTelefono() {
         return telefono;
     }
 
+    /**
+     * Método que establece el teléfono
+     * @param telefono
+     */
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
+    /**
+     * Método que regresa los nombres
+     * @return nombres
+     */
     public String getNombres() {
         return nombres;
     }
 
+    /**
+     * Método que establece los nombres
+     * @param nombres
+     */
     public void setNombres(String nombres) {
         this.nombres = nombres;
     }
 
+    /**
+     * Método que regresa el apellido paterno
+     * @return apellidoPaterno
+     */
     public String getApellidoPaterno() {
         return apellidoPaterno;
     }
 
+    /**
+     * Método que establece el apellido paterno
+     * @param apellidoPaterno
+     */
     public void setApellidoPaterno(String apellidoPaterno) {
         this.apellidoPaterno = apellidoPaterno;
     }
 
+    /**
+     * Método que regresa el apellido materno
+     * @return apellidoMaterno
+     */
     public String getApellidoMaterno() {
         return apellidoMaterno;
     }
 
+    /**
+     * Método que establece el apellido materno
+     * @param apellidoMaterno
+     */
     public void setApellidoMaterno(String apellidoMaterno) {
         this.apellidoMaterno = apellidoMaterno;
     }
 
+    /**
+     * Método que regresa las licencias
+     * @return licencias
+     */
     public List<Licencia> getLicencias() {
         return licencias;
     }
 
+    /**
+     * Método que establece las licencias
+     * @param licencias
+     */
     public void setLicencias(List<Licencia> licencias) {
         this.licencias = licencias;
     }
 
+    /**
+     * Método que regresa los vehículos
+     * @return vehiculos
+     */
     public List<Vehiculo> getVehiculos() {
         return vehiculos;
     }
 
+    /**
+     * Método que establece los vehículos
+     * @param vehiculos
+     */
     public void setVehiculos(List<Vehiculo> vehiculos) {
         this.vehiculos = vehiculos;
     }
 
+    /**
+     * Método que regresa el hash
+     * @return hash
+     */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -124,6 +266,11 @@ public class Persona implements Serializable {
         return hash;
     }
 
+    /**
+     * Método que compara objetos
+     * @param object
+     * @return true si son iguales, false si no
+     */
     @Override
     public boolean equals(Object object) {
         if (!(object instanceof Persona)) {
@@ -136,9 +283,14 @@ public class Persona implements Serializable {
         return true;
     }
 
+    /**
+     * Método que regresa el objeto en forma de cadena
+     * @return cadena
+     */
     @Override
     public String toString() {
-        return "bda.itson.entidadesJPA.Persona[ id=" + id + " ]";
+        return "Persona [id=" + id + ", apellidoMaterno=" + apellidoMaterno + ", apellidoPaterno=" + apellidoPaterno
+                + ", esDiscapacitado=" + esDiscapacitado + ", fechaNacimiento=" + fechaNacimiento + ", licencias="
+                + licencias + ", nombres=" + nombres + ", telefono=" + telefono + ", vehiculos=" + vehiculos + "]";
     }
-    
 }
