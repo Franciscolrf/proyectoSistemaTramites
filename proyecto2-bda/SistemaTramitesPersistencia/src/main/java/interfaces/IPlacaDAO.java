fae5b57efb7cc28688369a83d3886ec4c28524f3
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-03-23 01:53:42 -0700

    Implementación
    Implementación de la interfaz IPlacaDAO

--- proyecto2-bda/SistemaTramitesPersistencia/src/main/java/interfaces/IPlacaDAO.java
+++ proyecto2-bda/SistemaTramitesPersistencia/src/main/java/interfaces/IPlacaDAO.java
@@ -4,10 +4,25 @@
  */
 package interfaces;
 
+import bda.itson.entidadesJPA.Placa;
+
 /**
  *
  * @author ID145
  */
 public interface IPlacaDAO {
+    /**
+     * Método que agrega una placa
+     * @param placa
+     * @return
+     */
+    public Placa agregar (Placa placa);
+
+    /**
+     * Método para consultar una placa por su id.
+     * @param idPlaca
+     * @return
+     */
+    public Placa consultar(Long idPlaca);
     
 }
