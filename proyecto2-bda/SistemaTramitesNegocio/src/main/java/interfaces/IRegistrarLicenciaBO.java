5d3761552703fa7199f2a0700e3960d8ed0d6110
abel11604 <abelcorona.quintero@gmail.com>
2024-03-25 04:18:33 -0700

    Comentariar interfaces

--- proyecto2-bda/SistemaTramitesNegocio/src/main/java/interfaces/IRegistrarLicenciaBO.java
+++ proyecto2-bda/SistemaTramitesNegocio/src/main/java/interfaces/IRegistrarLicenciaBO.java
@@ -13,11 +13,30 @@ import java.util.List;
 /**
  *
  * @author fabri
+ *
+ * Interfaz que define las operaciones para el registro y búsqueda de licencias.
  */
 public interface IRegistrarLicenciaBO {
 
+    /**
+     * Método para registrar una licencia en el sistema.
+     *
+     * @param licencia La licencia a registrar.
+     */
     void registrarLicencia(LicenciaDTO licencia);
 
+    /**
+     * Método para buscar personas en el sistema según un parámetro de búsqueda.
+     *
+     * @param buscarParametro El parámetro de búsqueda para buscar personas.
+     * @return Una lista de objetos PersonaDTO que coinciden con el parámetro de búsqueda.
+     */
     List<PersonaDTO> buscarPersonas(String buscarParametro);
+
+    /**
+     * Método para asignar valores a una licencia antes de ser registrada.
+     *
+     * @param licenciaDTO La licencia a la cual se le asignarán los valores.
+     */
     void asignarValoresLicencia(LicenciaDTO licenciaDTO);
 }
--- proyecto2-bda/SistemaTramitesNegocio/src/main/java/interfaces/IregistrarPersona.java
+++ proyecto2-bda/SistemaTramitesNegocio/src/main/java/interfaces/IregistrarPersona.java
@@ -8,13 +8,13 @@ import dtos.PersonaDTO;
 import java.util.List;
 
 /**
- * @author abelc
- * Interfaz para el registro de personas en el sistema.
+ * @author abelc Interfaz para el registro de personas en el sistema.
  */
 public interface IregistrarPersona {
 
     /**
      * Método para realizar un registro masivo de personas en el sistema.
+     *
      * @param personas Lista de objetos PersonaDTO a registrar.
      * @return True si el registro fue exitoso, False si ocurrió algún error.
      */
@@ -22,10 +22,18 @@ public interface IregistrarPersona {
 
     /**
      * Método para generar una lista de personas con datos aleatorios.
+     *
      * @param cantidad Cantidad de personas a generar.
      * @return Lista de objetos PersonaDTO generados.
      */
-    public List<PersonaDTO> generarlista(int cantidad);   
-    
+    public List<PersonaDTO> generarlista(int cantidad);
+
+    /**
+     * Busca una persona por su RFC y la devuelve como un DTO de Persona.
+     *
+     * @param rfc El RFC de la persona a buscar.
+     * @return El DTO de Persona que corresponde al RFC especificado, si se
+     * encuentra; de lo contrario, devuelve null.
+     */
     public PersonaDTO buscarRFC(String rfc);
 }
