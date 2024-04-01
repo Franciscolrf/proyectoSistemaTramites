aaa93e08b1ce65849c002681b4b8c7005624f37c
abel11604 <abelcorona.quintero@gmail.com>
2024-04-01 02:35:25 -0700

    registrar placas

---
+++ proyecto2-bda/SistemaTramitesNegocio/src/main/java/negocio/RegistrarPlaca.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package negocio;
+
+import interfaces.IConexion;
+
+/**
+ *
+ * @author abelc
+ */
+public class RegistrarPlaca {
+     /**
+     * Unidad de persistencia para la conexión JPA.
+     */
+    String unidadPersistencia = "bda.itson_SistemaTramitesPersistencia_jar_1.0-SNAPSHOTPU";
+
+    /**
+     * Objeto para la conexión JPA.
+     */
+    IConexion conexionJPA; 
+}
