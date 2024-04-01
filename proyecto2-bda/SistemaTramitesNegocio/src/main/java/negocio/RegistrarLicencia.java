407da76af83980af372613bab29960bca1109432
abel11604 <abelcorona.quintero@gmail.com>
2024-04-01 02:36:12 -0700

    implementacion de clase de conversiones

--- proyecto2-bda/SistemaTramitesNegocio/src/main/java/negocio/RegistrarLicencia.java
+++ proyecto2-bda/SistemaTramitesNegocio/src/main/java/negocio/RegistrarLicencia.java
@@ -18,38 +18,43 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import otros.Conversiones;
 
 /**
  *
  * @author fabri
  */
 public class RegistrarLicencia implements IRegistrarLicenciaBO {
-    
+
     private final ILicencia licenciaDAO;
-    PersonaDAO personaDAO;
+    private final Conversiones conversiones;
 
     public RegistrarLicencia() {
         this.licenciaDAO = new LicenciaDAO();
-        this.personaDAO = new PersonaDAO();
-       
+        this.conversiones = new Conversiones();
+
     }
-     /**
-     * Registra una licencia a partir de un objeto LicenciaDTO.
-     * Convierte el objeto DTO a un objeto Licencia y lo pasa al LicenciaDAO para su registro.
-     * 
-     * @param licenciaDTO Objeto LicenciaDTO que contiene la información de la licencia a registrar.
+
+    /**
+     * Registra una licencia a partir de un objeto LicenciaDTO. Convierte el
+     * objeto DTO a un objeto Licencia y lo pasa al LicenciaDAO para su
+     * registro.
+     *
+     * @param licenciaDTO Objeto LicenciaDTO que contiene la información de la
+     * licencia a registrar.
      */
     @Override
     public void registrarLicencia(LicenciaDTO licenciaDTO) {
-        
+
         try {
-            Licencia licencia = convertirALicencia(licenciaDTO);
-            
+            Licencia licencia=conversiones.LicenciaDTOALicencia(licenciaDTO);
+
             licenciaDAO.registrarLicencia(licencia);
         } catch (PersistenciaException ex) {
             Logger.getLogger(RegistrarLicencia.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
 
 //    private LicenciaDTO convertirALicenciaDTO(Licencia licencia) {
@@ -62,27 +67,14 @@ public class RegistrarLicencia implements IRegistrarLicenciaBO {
 //        licenciaDTO.setPersona(licencia.getPersona());
 //        return licenciaDTO;
 //    }
-    
-     /**
-     * Busca personas según un parámetro de búsqueda y devuelve una lista de objetos PersonaDTO.
-     * 
-     * @param buscarParametro Parámetro de búsqueda para encontrar personas.
-     * @return Lista de objetos PersonaDTO que satisfacen el criterio de búsqueda.
-     */
-    private Licencia convertirALicencia(LicenciaDTO licenciaDTO) {
-        Licencia licencia = new Licencia();
-        licencia.setCosto(licenciaDTO.getCosto());
-        licencia.setFechaExpedicion(licenciaDTO.getFechaExpedicion());
-        licencia.setFechaVencimiento(licenciaDTO.getFechaVencimiento());
-        licencia.setPersona(convertirAPersona(licenciaDTO.getPersona()));
-        licencia.setVigencia(licenciaDTO.getVigencia());
-        return licencia;
-    }
+ 
     /**
-     * Busca personas según un parámetro de búsqueda y devuelve una lista de objetos PersonaDTO.
-     * 
+     * Busca personas según un parámetro de búsqueda y devuelve una lista de
+     * objetos PersonaDTO.
+     *
      * @param buscarParametro Parámetro de búsqueda para encontrar personas.
-     * @return Lista de objetos PersonaDTO que satisfacen el criterio de búsqueda.
+     * @return Lista de objetos PersonaDTO que satisfacen el criterio de
+     * búsqueda.
      */
     @Override
     public List<PersonaDTO> buscarPersonas(String buscarParametro) {
@@ -98,52 +90,17 @@ public class RegistrarLicencia implements IRegistrarLicenciaBO {
 
         // Convertir cada persona a DTO y agregarla a la lista de DTOs
         for (Persona persona : personasRegistradas) {
-            PersonaDTO personaDTO = convertirAPersonaDTO(persona);
+            PersonaDTO personaDTO = conversiones.convertirAPersonaDTO(persona);
             personasRegistradasDTO.add(personaDTO);
         }
-        
+
         return personasRegistradasDTO;
     }
-    
-     /**
-     * Convierte un objeto PersonaDTO a un objeto Persona.
-     * 
-     * @param personaDTO Objeto PersonaDTO a convertir.
-     * @return Objeto Persona convertido.
-     */
-    private Persona convertirAPersona(PersonaDTO personaDTO) {
-        Persona persona = new Persona();
-        persona.setId(personaDTO.getId());
-        persona.setNombres(personaDTO.getNombres());
-        persona.setApellidoPaterno(personaDTO.getApellidoPaterno());
-        persona.setApellidoMaterno(personaDTO.getApellidoMaterno());
-        persona.setFechaNacimiento(personaDTO.getFechaNacimiento());
-        persona.setRFC(personaDTO.getRFC());
-        persona.setTelefono(personaDTO.getTelefono());
-        persona.setEsDiscapacitado(personaDTO.isEsDiscapacitado());
-        return persona;
-    }
-     /**
-     * Convierte un objeto Persona a un objeto PersonaDTO.
-     * 
-     * @param persona Objeto Persona a convertir.
-     * @return Objeto PersonaDTO convertido.
-     */
-    private PersonaDTO convertirAPersonaDTO(Persona persona) {
-        PersonaDTO personaDTO = new PersonaDTO();
-        personaDTO.setId(persona.getId());
-        personaDTO.setNombres(persona.getNombres());
-        personaDTO.setApellidoPaterno(persona.getApellidoPaterno());
-        personaDTO.setApellidoMaterno(persona.getApellidoMaterno());
-        personaDTO.setFechaNacimiento(persona.getFechaNacimiento());
-        personaDTO.setRFC(persona.getRFC());
-        personaDTO.setTelefono(persona.getTelefono());
-        personaDTO.setEsDiscapacitado(persona.isEsDiscapacitado());
-        return personaDTO;
-    }
+
     /**
-     * Asigna valores a una licencia según su vigencia y la condición de discapacidad de la persona.
-     * 
+     * Asigna valores a una licencia según su vigencia y la condición de
+     * discapacidad de la persona.
+     *
      * @param licenciaDTO Objeto LicenciaDTO al que se le asignarán los valores.
      */
     @Override
@@ -174,6 +131,14 @@ public class RegistrarLicencia implements IRegistrarLicenciaBO {
             }
         }
     }
-    
-    
+
+    public boolean personaTieneLicenciaActiva(PersonaDTO personadto) {
+        Persona persona = conversiones.PersonaDTOAPersona(personadto);
+        try {
+            Licencia licenciaActiva = licenciaDAO.obtenerLicenciaActiva(persona);
+            return licenciaActiva != null;
+        } catch (PersistenciaException ex) {
+            return false;
+        }
+    }
 }
