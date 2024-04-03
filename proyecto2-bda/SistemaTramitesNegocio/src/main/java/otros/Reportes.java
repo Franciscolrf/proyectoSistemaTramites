04e514640d16ca880c3b1634c962798f007fda6c
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-04-03 03:23:43 -0700

    Jasper reports es muy feo XD

--- proyecto2-bda/SistemaTramitesNegocio/src/main/java/otros/Reportes.java
+++ proyecto2-bda/SistemaTramitesNegocio/src/main/java/otros/Reportes.java
@@ -1,18 +1,12 @@
 package otros;
-import net.sf.jasperreports.engine.*;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 /**
  * Clase que proporciona métodos para generar reportes de trámites.
- * Utilizando la libreria JasperReports.
+ * 
  * @autor Francisco LR
  */
 public class Reportes {
 
-
+    
     
 }
